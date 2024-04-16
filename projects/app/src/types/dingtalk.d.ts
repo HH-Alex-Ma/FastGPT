1f93f38eadb21362911ac57e86cf8d58b15939cc
Damon Yang <53988941+damonyang007@users.noreply.github.com>
2024-04-16 15:09:08 +0800

    fix: 修复自动化部署流程报错

--- projects/app/src/types/dingtalk.d.ts
+++ projects/app/src/types/dingtalk.d.ts
@@ -1,2 +1,9 @@
-declare module '@alicloud/dingtalk/oauth2_1_0';
-declare module '@alicloud/dingtalk/contact_1_0';
+declare module '@alicloud/dingtalk/oauth2_1_0' {
+  const dingtalkoauth2_1_0: any;
+  export = dingtalkoauth2_1_0;
+}
+
+declare module '@alicloud/dingtalk/contact_1_0' {
+  const dingtalkcontact_1_0: any;
+  export = dingtalkcontact_1_0;
+}
