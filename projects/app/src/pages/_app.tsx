147e7f4876aaa45c61a69fbf53bc454ae1becf51
Damon Yang <53988941+damonyang007@users.noreply.github.com>
2024-05-21 10:33:13 +0800

    fix: 展示著作控制台标识

--- projects/app/src/pages/_app.tsx
+++ projects/app/src/pages/_app.tsx
@@ -53,16 +53,16 @@ function App({ Component, pageProps }: AppProps) {
       setTitle(systemTitle || 'GenAI企业应用助手');
 
       // log fastgpt
-      // if (!isPlus) {
-      //   console.log(
-      //     '%cWelcome to FastGPT',
-      //     'font-family:Arial; color:#3370ff ; font-size:18px; font-weight:bold;',
-      //     `GitHub：https://github.com/labring/FastGPT`
-      //   );
-      // }
-      // if (show_git) {
-      //   loadGitStar();
-      // }
+      if (!isPlus) {
+        console.log(
+          '%cWelcome to FastGPT',
+          'font-family:Arial; color:#3370ff ; font-size:18px; font-weight:bold;',
+          `GitHub：https://github.com/labring/FastGPT`
+        );
+      }
+      if (show_git) {
+        loadGitStar();
+      }
 
       setScripts(scripts || []);
       setInitd();
