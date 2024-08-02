d73bb3f57e63b9fda50b93612c2b785a01d54422
HH-Alex-Ma <alex.ma@huameisoft.cn>
2024-08-02 13:58:32 +0800

    增加面试助手到twoColConfig

--- projects/app/src/components/ChatBox/components/twoColAppConfig.tsx
+++ projects/app/src/components/ChatBox/components/twoColAppConfig.tsx
@@ -4,7 +4,7 @@ import { twoColAppType } from '@fastgpt/global/core/app/constants';
 export const twoColConfigList: twoColAppType[] = [
   {
     id: '66962371a1e76a2b79b2b86e',
-    name: '小红书营销专家(aitest)',
+    name: '小红书营销专家-aitest',
 
     left_tag: '产品推广方案',
     left_tabs: ['会话'],
@@ -16,7 +16,7 @@ export const twoColConfigList: twoColAppType[] = [
   },
   {
     id: '66962497a1e76a2b79b2b929',
-    name: 'AI智能搜索(aitest)',
+    name: 'AI智能搜索-aitest',
 
     left_tag: '问题回答',
     left_tabs: ['会话'],
@@ -28,7 +28,7 @@ export const twoColConfigList: twoColAppType[] = [
   },
   {
     id: '66a87c8825f9aec47370bcbe',
-    name: '小红书营销专家(aitest热点)',
+    name: '小红书营销专家(热点)-aitest',
 
     left_tag: '热点分析',
     left_tabs: ['会话'],
@@ -40,7 +40,7 @@ export const twoColConfigList: twoColAppType[] = [
   },
   {
     id: '66a89ccca44bac3435d64493',
-    name: '小红书营销专家(热点)',
+    name: '小红书营销专家(热点)-local',
 
     left_tag: '热点分析',
     left_tabs: ['会话'],
@@ -49,5 +49,17 @@ export const twoColConfigList: twoColAppType[] = [
     right_tag: '文案创作',
     right_tabs: ['会话'],
     right_module: '文案创作'
+  },
+  {
+    id: '66ac748136f5ee8c7f27096b',
+    name: '智能面试助手-aitest',
+
+    left_tag: '候选人评估',
+    left_tabs: ['会话'],
+    left_module: '候选人评估',
+
+    right_tag: '面试问题设计',
+    right_tabs: ['会话'],
+    right_module: '面试问题设计'
   }
 ];
