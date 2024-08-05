f0a5276dfcb8c601746961cd68bc94e15da9f752
Alex Ma <alex.ma@huameisoft.cn>
2024-08-05 13:48:48 +0800

    Fix: 修改热点搜索为外部接口请求

--- projects/app/src/components/ChatBox/index.tsx
+++ projects/app/src/components/ChatBox/index.tsx
@@ -957,15 +957,13 @@ const ChatBox = (
                       <Flex flexDirection="row" width="100%">
                         {/* 引用 */}
                         <Flex flexDirection="column" width="100%">
-                          {' '}
                           {/* 100%因为目前只显示一个引用tag */}
                           <ResponseTags
                             flowResponses={item.responseData}
                             showDetail={!shareId && !teamId}
                           />
                         </Flex>
-                        {/* 外部引用 */}{' '}
-                        {/*暂时不需要外部引用/*}
+                        {/* 外部引用 */} {/*暂时不需要外部引用*/}
                         {/*
                         <Flex flexDirection="column" width="50%">
                           <ExternalReponse
